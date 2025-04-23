1e1948ea725802cdf61f2549c3b432d18aad3221
DiViGaa <nik.andreenkon@gmail.com>
2025-04-23 21:07:29 +0300

    ShopAndMenuScreen

--- Assets/Scripts/MainGameScreen.cs
+++ Assets/Scripts/MainGameScreen.cs
@@ -1,22 +1,38 @@
-using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class MainGameScreen : MonoBehaviour
 {
     [SerializeField] private View view;
     [SerializeField] private Button clickButton;
-
+    [SerializeField] private Button MenuButton;
+    [SerializeField] private Button ShopButton;
+    
+    public event UnityAction OnClickMenuButton;
+    public event UnityAction OnClickShopButton;
+    
+    private UnityAction _menuButtonCallback;
+    private UnityAction _shopButtonCallback;
+    
     private AbstractController _controller;
 
     private void OnEnable()
     {
+        _menuButtonCallback = () => OnClickMenuButton?.Invoke();
+        _shopButtonCallback = () => OnClickShopButton?.Invoke();
         clickButton.onClick.AddListener(ControllerClick);
+        MenuButton.onClick.AddListener(_menuButtonCallback);
+        ShopButton.onClick.AddListener(_shopButtonCallback);
     }
 
     private void OnDisable()
     {
         clickButton.onClick.RemoveAllListeners();
+        MenuButton.onClick.RemoveAllListeners();
+        ShopButton.onClick.RemoveAllListeners();
+        _menuButtonCallback = null;
+        _shopButtonCallback = null;
     }
 
     private void ControllerClick()
