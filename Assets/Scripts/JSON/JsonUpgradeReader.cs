cfa4ab2193d8f02aae0d98cba177d7a63bcd5dd4
DiViGaa <131615743+DiViGaa@users.noreply.github.com>
2025-05-14 16:10:09 +0300

    Newtonsoft.Json

---
+++ Assets/Scripts/JSON/JsonUpgradeReader.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using Upgrades;
+using File = System.IO.File;
+
+
+
+namespace JSON
+{
+    public static class JsonUpgradeReader
+    {
+        public static List<Upgrade> LoadJson(string fileName)
+        {
+            string url = string.Empty;
+            url = "file://" + Application.dataPath + "/Resources/JSON/" + fileName;
+
+            string json = File.ReadAllText(url);
+            
+            List<Upgrade> upgrades = JsonConvert.DeserializeObject<List<Upgrade>>(json);
+            return upgrades;
+        }
+    }
+}
--- Assets/Scripts/Upgrades/Upgrade.cs
+++ Assets/Scripts/Upgrades/Upgrade.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 namespace Upgrades
 {
+    [System.Serializable]
     public class Upgrade : MonoBehaviour
     {
-        private string _name;
-        private string _description;
-        private int _id;
-        private int _cost;
-        private int _minLevel;
-        private int _maxLevel;
-        private int _currentLevel;
-        private bool _isBuyed;
+        public string Name {get; private set;}
+        public string Description{get; private set;}
+        public int Id {get; private set;}
+        public int Cost {get; private set;}
+        public int MinLevel {get; private set;}
+        public int MaxLevel {get; private set;}
+        public int CurrentLevel {get; private set;}
+        public bool IsBuyed {get; private set;}
+        public string ImagePath {get; private set;}
 
     }
 }
